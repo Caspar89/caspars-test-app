3e3b09307cb07b84bc51ff3d3cf8398a9852ef83
Caspar89 <ccavandeursen@gmail.com>
2020-04-23 13:16:57 +0100

    moved compiler cli to dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-component',
+  templateUrl: './app.component.html'
 })
+@Injectable()
 export class AppComponent {
-  title = 'app works!';
+  constructor(private appService: AppService) {}
+  public test_1 = "";
+  public test_2 = "";
+  public test_web = "";
+ 
+  ngOnInit() {
+    this.subscribeToSubject();
+    this.subscribeToWebSubject();
+    setTimeout(() => { this.appService.setSubject("Hi! was changed from the main component"); }, 5000);
+  }
+
+  private subscribeToSubject(): void {
+    this.appService.getSubject().subscribe({
+      next: (v) => { this.test_1 = v }
+    });
+    this.appService.getSubject().subscribe({
+      next: (v) => { this.test_2 = v }
+    });
+  }
+
+  private subscribeToWebSubject(): void {
+    this.appService.getWebSubject().subscribe({
+      next: (v) => { this.test_web = v }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppService } from './app.service';
+import { AppComponentSecondary } from './app.component.secondary';
+import { AppRoutingModule } from './app.routing.module';
+import { AppRoot } from './app.root';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppComponentSecondary,
+    AppRoot
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    AppService
+  ],
+  bootstrap: [
+    AppRoot
+  ]
 })
 export class AppModule { }
